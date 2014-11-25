5e1bf2792cfbfbe12ff67800b42948eed109d8df
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-25 18:42:06 +0100

    Met les cartes restantes dans la pioche

--- src/fr/utt/isi/lo02/projet/modele/Scrambler.java
+++ src/fr/utt/isi/lo02/projet/modele/Scrambler.java
@@ -15,15 +15,36 @@ public class Scrambler extends Joueur {
 	}
 	
 	
-	public void distribuerPaquet(JeuDeCartes jdc, ArrayList<Joueur> joueurs) {
+	public void distribuerPaquet(Bataille bataille) {
+		JeuDeCartes jdc = bataille.getJeuDeCartes();
+		 ArrayList<Joueur> joueurs = bataille.getJoueurs();
+		
+		
+		
 		//tant que le paquet n'est pas vide
-		while (!jdc.estVide()) {
+		while (!jdc.estVide() || jdc.size() == joueurs.size() * 9) {
 			//Iterator de joueur
 			Iterator<Joueur> itj = joueurs.iterator();
 			while (itj.hasNext()) {		
 				((Joueur) itj.next()).recevoirCarte(jdc.tirerCarteDessus());
 			}
 		}
+		
+		//Met le reste dans le tas
+		if (!jdc.estVide()) {
+			ArrayList<Carte> pioche = new ArrayList<>();
+			Iterator<Carte> it = jdc.getTas().iterator();
+			while(it.hasNext())
+				pioche.add(it.next());
+			
+			bataille.setPioche(pioche);
+		}
+		
+			
+		
+		
+	
+		//TODO jdc.setPioche(cequilreste)
 	}
 	
 
