0c391a12055b180338e7d93d9cbda548a22898f7
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-25 16:06:01 +0100

    Ajoute la Javadoc

--- src/fr/utt/isi/lo02/projet/modele/Joueur.java
+++ src/fr/utt/isi/lo02/projet/modele/Joueur.java
@@ -4,17 +4,27 @@ import java.util.LinkedList;
 
 /**
  * Représente le Joueur
- * @author daussy
+ * @author daussy - obeidat
  *
  */
 public class Joueur {
-
+	
+	/** Le nom du joueur */
 	private String nom;
+	/** Les cartes faces visibles du joueur */
 	private final LinkedList<Carte> faceUp;
+	/** Les cartes faces cachées du joueur */
 	private final LinkedList<Carte> faceDown;
+	/** Les cartes en main du joueur */
 	private final LinkedList<Carte> main;
+	/** La stratégie adoptée par le joueur */
 	private final StrategieJeu strategie;
 	
+	
+	/**
+	 * Constructeur du joueur
+	 * @param nom Nom du joueur
+	 */
 	public Joueur(String nom){
 		this.strategie = null;//TODO : changer
 		this.nom = nom;
@@ -23,24 +33,53 @@ public class Joueur {
 		this.main = new LinkedList<>();
 	}
 
+	
+	/**
+	 * Getter de nom
+	 * @return nom du joueur
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * Setter de nom
+	 * @param nom nom du joueur
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+	/**
+	 * Getter de FaceUp
+	 * @return liste des cartes face visible
+	 */
 	public LinkedList<Carte> getFaceUp() {
 		return faceUp;
 	}
-
+	
+	/**
+	 * Getter de FaceDown
+	 * @return liste des cartes face cachée
+	 */
 	public LinkedList<Carte> getFaceDown() {
 		return faceDown;
 	}
 
+	/**
+	 * Getter de la main
+	 * @return les cartes dans la main du joueur
+	 */
 	public LinkedList<Carte> getMain() {
 		return main;
+	}
+
+	/**
+	 * Getter de la stratégie
+	 * @return la strategie adoptée
+	 */
+	public StrategieJeu getStrategie() {
+		return strategie;
 	}
 
 	
