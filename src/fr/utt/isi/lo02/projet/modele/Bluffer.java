fb8be2357ff737344293d571e5114b32a900ccce
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-25 17:26:42 +0100

    Ajoute classes Défausser et Bluffer qui héritent de l'interface StratégieJeu

---
+++ src/fr/utt/isi/lo02/projet/modele/Bluffer.java
@@ -0,0 +1,11 @@
+package fr.utt.isi.lo02.projet.modele;
+
+public class Bluffer implements StrategieJeu {
+
+	@Override
+	public void jouerCarte() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
