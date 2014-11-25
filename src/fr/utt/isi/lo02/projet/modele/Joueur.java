bee7cf90d0cc8f6af7cd512ee917cc5cf3daf4d1
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-25 18:46:12 +0100

    Modifie la méthode toString

--- src/fr/utt/isi/lo02/projet/modele/Joueur.java
+++ src/fr/utt/isi/lo02/projet/modele/Joueur.java
@@ -32,6 +32,15 @@ public class Joueur {
 		this.faceDown = new LinkedList<>();
 		this.main = new LinkedList<>();
 	}
+	
+	
+	/**
+	 * Permet au joueur de jouer
+	 * (appelle la méthode propre à sa stratégie)
+	 */
+	public void jouer() {
+		strategie.jouerCarte();
+	}
 
 	
 	/**
@@ -80,6 +89,40 @@ public class Joueur {
 	 */
 	public StrategieJeu getStrategie() {
 		return strategie;
+	}
+
+	/**
+	 * Permet de recevoir une carte lors de la distribution
+	 * @param carte Carte à recevoir
+	 */
+	public void recevoirCarte(Carte carte) {
+		//On vérifie dans quelle liste la mettre		
+		if (faceDown.size() < 3)
+			faceDown.add(carte);
+		
+		else if (faceUp.size() < 3)
+			faceUp.add(carte);
+		
+		else if (main.size() < 3)
+			main.add(carte);
+		
+		//else mettre dans la pioche d'une façon ou d'une autre
+	}
+	
+	
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("\n");
+		sb.append(nom);
+		sb.append("\n\t Main -> ");
+		sb.append(main);
+		sb.append("\n\t FaceUp -> ");
+		sb.append(faceUp);
+		sb.append("\n\t FaceDown -> ");
+		sb.append(faceDown);
+		sb.append("\n");
+		return sb.toString();
 	}
 
 	
