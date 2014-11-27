7a384070984f9e076f97749e0f40d2f8d94ea1e6
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-27 11:57:38 +0100

    Crée des nouveaux joueurs

--- src/JeuConsole.java
+++ src/JeuConsole.java
@@ -7,24 +7,27 @@ public class JeuConsole {
 	
 	
 	public static void main(String[] args) {
-		
-	
-		Bataille bataille = new Bataille();
 
 		Joueur Benjamin = new Joueur("Benjamin");
 		Joueur Diane = new Joueur("Diane");
 		Joueur Maxime = new Joueur("Maxime");
+		Joueur Thomas = new Joueur("Thomas");
+		Joueur Lou = new Joueur("Lou");
 		Scrambler Alexandre = new Scrambler("Alexandre");
 		
+		Bataille bataille = new Bataille();
 		
 		bataille.addJoueur(Benjamin);
 		bataille.addJoueur(Diane);
 		bataille.addJoueur(Maxime);
+		bataille.addJoueur(Thomas);
+		bataille.addJoueur(Lou);
 		bataille.addJoueur(Alexandre);
 		
 		Alexandre.distribuerPaquet(bataille);
 		
 		System.out.println(bataille.toString());
+		//System.out.println(Joueur.NB_JOUEURS);
 		
 	
 	}
