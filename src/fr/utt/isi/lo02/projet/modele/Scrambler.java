28ef1abb3143afbe1cb4fe561610bc44c2d6cd7b
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-27 10:38:04 +0100

    Arrête la distribution des cartes lorsque chq joueur en a 9

--- src/fr/utt/isi/lo02/projet/modele/Scrambler.java
+++ src/fr/utt/isi/lo02/projet/modele/Scrambler.java
@@ -18,28 +18,34 @@ public class Scrambler extends Joueur {
 	public void distribuerPaquet(Bataille bataille) {
 		JeuDeCartes jdc = bataille.getJeuDeCartes();
 		jdc.melanger();
-		 ArrayList<Joueur> joueurs = bataille.getJoueurs();
+		ArrayList<Joueur> joueurs = bataille.getJoueurs();
+		int nbCartesDistribuees = 0;
 		
 		
-		
-		//tant que le paquet n'est pas vide
-		while (!jdc.estVide() || jdc.size() == joueurs.size() * 9) {
+		// tant que le paquet n'est pas vide
+		while (!jdc.estVide()) {
 			//Iterator de joueur
 			Iterator<Joueur> itj = joueurs.iterator();
 			while (itj.hasNext()) {		
 				((Joueur) itj.next()).recevoirCarte(jdc.tirerCarteDessus());
+				nbCartesDistribuees++;
 			}
+			//Si on a distribué 9 cartes à chaque joueur on peut sortir de la boucle
+			//et mettre tout le reste des cartes dans la pioche
+			if (nbCartesDistribuees == (joueurs.size() * 9))
+				//Met le reste dans le tas s'il reste des cartes
+				if (!jdc.estVide()) {
+					ArrayList<Carte> pioche = new ArrayList<>();
+					Iterator<Carte> it = jdc.getTas().iterator();
+					while(it.hasNext())
+						pioche.add(it.next());
+					
+					bataille.setPioche(pioche);
+				}
+				break;
 		}
 		
-		//Met le reste dans le tas
-		if (!jdc.estVide()) {
-			ArrayList<Carte> pioche = new ArrayList<>();
-			Iterator<Carte> it = jdc.getTas().iterator();
-			while(it.hasNext())
-				pioche.add(it.next());
-			
-			bataille.setPioche(pioche);
-		}
+		
 		
 		
 	}
