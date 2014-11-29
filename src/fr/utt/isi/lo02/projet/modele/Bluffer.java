5a0da4ea96d4eedc568139bc6324c443eed9f096
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-29 23:45:57 +0100

    Ajoute syso pour faciliter dev

--- src/fr/utt/isi/lo02/projet/modele/Bluffer.java
+++ src/fr/utt/isi/lo02/projet/modele/Bluffer.java
@@ -3,8 +3,14 @@ package fr.utt.isi.lo02.projet.modele;
 public class Bluffer implements StrategieJeu {
 
 	@Override
-	public void jouerCarte() {
-		// TODO Auto-generated method stub
+	public void jouerCarte(Joueur joueur) {
+		System.out.println("il ne sait pas encore jouer seul");
+		
+	}
+
+	@Override
+	public void echangerCartes(Joueur joueur) {
+		System.out.println("La methode n'est pas encore implementee");
 		
 	}
 
--- src/fr/utt/isi/lo02/projet/modele/Defausser.java
+++ src/fr/utt/isi/lo02/projet/modele/Defausser.java
@@ -3,9 +3,13 @@ package fr.utt.isi.lo02.projet.modele;
 public class Defausser implements StrategieJeu {
 
 	@Override
-	public void jouerCarte() {
-		// TODO Auto-generated method stub
-		
+	public void jouerCarte(Joueur joueur) {
+		System.out.println("Il ne sait pas encore jouer seul");
+	}
+
+	@Override
+	public void echangerCartes(Joueur joueur) {
+		System.out.println("La methode n'est pas encore implementee");
 	}
 
 }
