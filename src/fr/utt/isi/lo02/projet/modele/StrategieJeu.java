a547ce0985de49e63dd9eea5c3e9decc2b03fbbf
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-29 23:47:21 +0100

    déplace echangerCarte

--- src/fr/utt/isi/lo02/projet/modele/StrategieJeu.java
+++ src/fr/utt/isi/lo02/projet/modele/StrategieJeu.java
@@ -1,7 +1,16 @@
 package fr.utt.isi.lo02.projet.modele;
 
+/**
+ * Interface Stratégie
+ * 3 stratégies différentes
+ * @author daussy
+ *
+ */
 public interface StrategieJeu {
+	
+	// TODO bouger la méthode échanger carte ici ?
+	public abstract void echangerCartes(Joueur joueur);
 
-	public abstract void jouerCarte();
+	public abstract void jouerCarte(Joueur joueur);
 	
 }
