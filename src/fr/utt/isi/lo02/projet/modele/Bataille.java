5222d0543712e4cba47e001dc92466d8c5388261
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-29 15:01:47 +0100

    Permet d'échanger ses cartes au début Tous les joueurs peuvent échanger leurs cartes (main & face visible) en début de partie.

--- src/fr/utt/isi/lo02/projet/modele/Bataille.java
+++ src/fr/utt/isi/lo02/projet/modele/Bataille.java
@@ -1,5 +1,7 @@
 package fr.utt.isi.lo02.projet.modele;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 
 public class Bataille {
@@ -40,6 +42,8 @@ public class Bataille {
 			
 	}
 	
+	
+	
 	/**
 	 * Permet de supprimer un joueur de la partie
 	 * @param j le joueur à spupprimer
@@ -69,7 +73,40 @@ public class Bataille {
 	public String toString() {
 		return joueurs.toString();
 	}
-	
+
+	/**
+	 * Demande à tous les joueurs s'ils veulent échanger leurs cartes
+	 * L'échange est possible seulement entre les cartes que le joueur
+	 * possède en main et les cartes visibles
+	 * Si le joueur répond ne pas vouloir faire d'échange on passe au joueur suivant
+	 * Si le joueur répond vouloir faire un échange, on lui propose l'échange et on
+	 * lui redemande s'il souhaite faire un échange jusqu'à ce qu'il dise non (considéré comme définitif)
+	 */
+	public void echangerCartes() {
+		Iterator<Joueur> it = this.joueurs.iterator();
+		int rep;
+		Scanner sc = new Scanner(System.in);
+
+		// Pour tous les joueurs
+		while (it.hasNext()) {
+			Joueur joueur = it.next();
+			rep = -1; // pour repasser dans la boucle
+
+			// Tant qu'ils n'ont pas répondu 'oui' ou 'non'
+			do {
+				// Tant qu'ils n'ont pas dit 'non'
+				do {
+					System.out.println("Veux-tu echanger des cartes "
+							+ joueur.getNom() + " ?\t(1 (oui)/ 2 (non))");
+					rep = sc.nextInt();
+					if (rep == 1)
+						joueur.proposerChangerCartes(); // Echange de cartes si
+														// réponse positive
+				} while (rep != 2);
+
+			} while (rep != 1 && rep != 2);
+		}
+	}
 	
 	
 	
