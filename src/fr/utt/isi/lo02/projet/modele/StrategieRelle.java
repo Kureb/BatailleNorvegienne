7e84fab8ce8b9afc7686171fa8b14ad76897b4ec
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-29 16:16:38 +0100

    Ajoute interface permettant de joueur manuellement

---
+++ src/fr/utt/isi/lo02/projet/modele/StrategieRelle.java
@@ -0,0 +1,16 @@
+package fr.utt.isi.lo02.projet.modele;
+
+/**
+ * Permet de définir comment va jouer un joueur réel
+ * @author daussy
+ *
+ */
+public class StrategieRelle implements StrategieJeu {
+
+	@Override
+	public void jouerCarte() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
