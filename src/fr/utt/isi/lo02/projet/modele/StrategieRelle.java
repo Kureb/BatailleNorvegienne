83ad8684c10f9b3f7054c9ac78666d1a2a67eb25
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-11-30 17:08:24 +0100

    Améliore jouerCarte(Joueur j) + choisirCarteAJouer

--- src/fr/utt/isi/lo02/projet/modele/StrategieRelle.java
+++ src/fr/utt/isi/lo02/projet/modele/StrategieRelle.java
@@ -1,5 +1,7 @@
 package fr.utt.isi.lo02.projet.modele;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
@@ -11,35 +13,105 @@ public class StrategieRelle implements StrategieJeu {
 
 	@Override
 	public void jouerCarte(Joueur joueur) {
-		//Chercher si y'a déjà un tas !
-		System.out.println("il joue");
-		if (Bataille.getInstance().getTable().isEmpty()) {
-			//si le tas est vide, on est donc le premier joueur à jouer
-			//Il peut poser 1, 2 ou 3 cartes sur la table (si elles ont la même valeur)
-			
-			//TODO lui laisser choisir la carte
-			joueur.poserCarteUnique(joueur.getMain().getFirst());
-			//joueur.poserCarte(joueur.getMain());
-			
-		} else {
-			//il faut jouer en fonction de la dernière carte posée sur le tas
-			System.out.println("Il faut implementer les tours suivant !!");
-			joueur.poserCarteUnique(joueur.getMain().getFirst());
-			
+		System.out.println(joueur);
+		ArrayList<Carte> cartes = this.choisirCarteAJoueur(joueur);
+		int taille = cartes.size(); //nb de cartes à piocher
+		Iterator<Carte> it = cartes.iterator();
+		while (it.hasNext()) {
+			joueur.poserCarteUnique(it.next());
+		}
+		
+		// Pour chq carte posée
+		for (int i = 0; i < taille; i++) {
+			// On redonne au joueur une carte de la pioche
+			joueur.recevoirCarte(Bataille.getInstance().getPioche().get(Bataille.getInstance().getPioche().size()-1));
+			Bataille.getInstance().getPioche().remove(Bataille.getInstance().getTable().size()-1);
 		}
 		
 		
-
+		
 	}
 
+	/**
+	 * Permet au joueur réel de choisir la carte qu'il veut jouer
+	 * @param joueur Joueur courant (réel)
+	 * @return Carte qu'il souhaite jouer
+	 */
+	public ArrayList<Carte> choisirCarteAJoueur(Joueur joueur) {
+		// L'arraylist qui contient les cartes qu'il veut poser
+		ArrayList<Carte> cartes = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		int choix = -1;
+		
+		
+		// Si la table n'est pas vide on lui montre la dernière carte jouée
+		if (!Bataille.getInstance().getTable().isEmpty()) {
+			int indexDernierElement = Bataille.getInstance().getTable().size()-1;
+			Carte derniereCarteJouee = Bataille.getInstance().getTable().get(indexDernierElement);
+			System.out.println("La derniere carte jouee etait : "
+					+ derniereCarteJouee);
+
+		}
+		
+		// On lui montre ses cartes et on demande ce qu'il veut jouer
+		System.out.println(joueur.getMain());
+
+		do {
+			System.out.print("Quelle carte veux-tu jouer ? ");
+			choix = sc.nextInt();
+		} while (choix > joueur.getMain().size());
+
+		choix--;
+		//On ajoute son choix à la liste des cartes que retourne la méthode
+		cartes.add(joueur.getMain().get(choix));
+
+		
+		// Ici on vérifie s'il a dans sa main une autre carte de la même valeur
+		// car il peut aussi la jouer
+		// Si tel est le cas, on lui demande s'il veut la jouer
+		// Si oui, on l'ajoute aussi à la liste de cartes retournée par la méthode
+		boolean autre = false;
+		Iterator<Carte> it = joueur.getMain().iterator();
+		int i = 1;
+		int reponse = 0;
+		while (it.hasNext()) {
+			Carte carteCourrante = it.next();
+			if (carteCourrante != cartes.get(0)) {
+				autre = (cartes.get(0)).aMemeValeur(carteCourrante);
+				if (autre) {
+					System.out.println("Tu as une carte de la meme valeur.");
+					do {
+						System.out.print("Veux-tu aussi poser " + carteCourrante + " (1 (oui)/2 (non)) ? ");
+						reponse = sc.nextInt();
+					} while (reponse != 1 && reponse != 2);
+					
+					if (reponse == 1) {
+						reponse = -1;
+						cartes.add(carteCourrante);
+					}
+						
+				}
+			}
+			i++;
+		}
+
+		return cartes;
+	}
+
+	
+	
+	/**
+	 * Permet de demander au joueur physique s'il
+	 * veut échanger des cartes ou non
+	 */
 	@Override
 	public void echangerCartes(Joueur joueur) {
-		// TODO trouver qqch pour éviter de passer l'objet Joueur en paramètre
+		System.out.println(joueur);
 		int rep;
 		Scanner sc = new Scanner(System.in);
 		do {
-			System.out.println("Veux tu echanger des cartes "
-					+ joueur.getNom() + "\t(1 (oui)/ 2 (non))");
+			System.out.print("Veux tu echanger des cartes "
+					+ joueur.getNom() + "\t(1 (oui)/ 2 (non)) ? ");
 			rep = sc.nextInt();
 			
 			if (rep == 1)
