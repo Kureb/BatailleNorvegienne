e4fa8b63cf0cc54a3fd11aa2a3d9b77a7d36f9c7
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2014-12-30 14:10:21 +0100

    Ajoute JDialog pr échanger cartes Joueur Réel
    Afin de mettre la Frame principale en pause lors l’échange de joueur
    réel, il faut passer par un JDialog
    Voilà donc un JDialog perso pour proposer au joueur d’échanger ses
    cartes !

---
+++ src/fr/utt/isi/lo02/projet/vue/DialogChangerCartes.java
@@ -0,0 +1,192 @@
+package fr.utt.isi.lo02.projet.vue;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Iterator;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import fr.utt.isi.lo02.projet.controleur.BatailleControleur;
+import fr.utt.isi.lo02.projet.controleur.ControleurAbstrait;
+import fr.utt.isi.lo02.projet.modele.Carte;
+import fr.utt.isi.lo02.projet.modele.Joueur;
+import fr.utt.isi.lo02.projet.modele.StrategieRelle;
+
+public class DialogChangerCartes extends JDialog {
+	
+	private JLabel icon;
+	private JTextField nom;
+	private Carte carteMain;
+	private Carte carteVisible;
+	private Joueur joueur;
+	private JButton valider;
+	
+	 public DialogChangerCartes(JFrame parent, String title, boolean modal, Joueur joueur){
+		    super(parent, title, modal);
+		    this.joueur = joueur;
+		    this.setSize(550, 270);
+		    this.setLocationRelativeTo(null);
+		    this.setResizable(false);
+		    this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		    this.initComponent();
+		  }
+
+	
+	 
+	 private void initComponent(){
+		    //Icône
+		    //icon = new JLabel(new ImageIcon("images/icone.jpg"));
+		    
+		 	
+		 
+		 	JPanel fenetre = new JPanel();
+		    fenetre.setBackground(Color.white);
+		    fenetre.setLayout(new GridLayout(0,1));
+		    //fenetre.add(icon);
+		    
+		  //Cartes en main
+		    JPanel main = new JPanel();
+			FlowLayout fl = new FlowLayout(FlowLayout.LEFT);
+			main.setLayout(fl);
+		
+			//TODO si on sélectionne une carte, déselectionner les autres..
+		
+			Iterator<Carte> it = joueur.getMain().iterator();
+		    while (it.hasNext()) {
+		    	final VueCarte vc = new VueCarte(it.next());
+		    	//final JLabel carte = vc.getImage();
+		    	if (joueur.getStrategie() instanceof StrategieRelle) {
+					vc.getImage().addMouseListener(new MouseAdapter() {
+						public void mouseClicked(MouseEvent me) {
+							if (vc.getImage().getBorder() == null) {
+								vc.getImage().setBorder(BorderFactory.createLineBorder(Color.black));
+								carteMain = new Carte(vc.getCarte().getValeur(), vc.getCarte().getCouleur());
+								//String msg = vc.getCarte().getValeur() + " " + vc.getCarte().getCouleur();
+								//valider.setText(msg);
+							} else {
+								vc.getImage().setBorder(null);
+								carteMain = null;
+							}	
+						}				
+					});
+				}
+		    	main.add(vc.getImage());
+		    }
+		    //Fin cartes en main
+		    
+		    //cartes visibles
+		    JPanel visibles = new JPanel();
+			FlowLayout fl2 = new FlowLayout(FlowLayout.LEFT);
+			visibles.setLayout(fl2);
+			
+			Iterator<Carte> it2 = joueur.getFaceUp().iterator();
+		    while (it2.hasNext()) {
+		    	final VueCarte vc = new VueCarte(it2.next());
+		    	//final JLabel carte = vc.getImage();
+		    	if (joueur.getStrategie() instanceof StrategieRelle) {
+					vc.getImage().addMouseListener(new MouseAdapter() {
+						public void mouseClicked(MouseEvent me) {
+							if (vc.getImage().getBorder() == null) {
+								vc.getImage().setBorder(BorderFactory.createLineBorder(Color.black));
+								carteVisible = new Carte(vc.getCarte().getValeur(), vc.getCarte().getCouleur());
+							} else {
+								vc.getImage().setBorder(null);
+								carteVisible = null;
+							}
+						}				
+					});
+				}
+		    	visibles.add(vc.getImage());
+		    }
+		    //fin cartes visibles
+
+		    JPanel control = new JPanel();
+		    final JButton okBouton = new JButton("Ok");
+		    
+		    okBouton.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent e) {  
+		    	  if (carteVisible != null && carteMain != null)
+		    		  //BatailleControleur.echange(joueur, carteVisible, carteMain);
+		    		  //joueur.echangerCarte(carteMain, carteVisible);
+		    	  	  setVisible(false);
+		    	  else
+		    		  okBouton.setText("invalide");
+		        
+		    	
+		      }
+		    });
+		    
+	
+
+		    JButton cancelBouton = new JButton("Annuler");
+		    cancelBouton.addActionListener(new ActionListener(){
+		      public void actionPerformed(ActionEvent arg0) {
+		    	  BatailleControleur.updateJTextArea("Echange annulé pour " + joueur.getNom());
+		        setVisible(false);
+		      }      
+		    });
+		    
+		    fenetre.add(main);
+		    fenetre.add(visibles);
+		    
+		    control.add(fenetre);
+		    control.add(okBouton);
+		    control.add(cancelBouton);
+
+		    this.getContentPane().add(control, BorderLayout.SOUTH);
+		  }
+
+
+
+	public Carte getCarteMain() {
+		return carteMain;
+	}
+
+
+
+	public void setCarteMain(Carte carteMain) {
+		this.carteMain = carteMain;
+	}
+
+
+
+	public Carte getCarteVisible() {
+		return carteVisible;
+	}
+
+
+
+	public void setCarteVisible(Carte carteVisible) {
+		this.carteVisible = carteVisible;
+	}
+
+
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}  
+  
+  
+  
+  
+  
+}
